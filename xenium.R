5f2f7a17c489c4bce6fc34a5f2e0944cea674739
Emre Arslan <emrearslan44@gmail.com>
2024-03-19 12:26:36 -0500

    Update xenium.R
    I wrote a piece of code to get all cell areas in vectorization format instead of for loop to save time. Please work on the function. I will check and correct once you are done.

--- xenium.R
+++ xenium.R
@@ -14,34 +14,35 @@ xenium_obj <- subset(xenium_obj, subset = nCount_Xenium > 0)
 xenium_read <- ReadXenium(path, outs = c("matrix", "microns"), type = "centroids", mols.qv.threshold = 20)
 
 
+# Run UMAP to reduce dimensions for visualization
+xenium_obj <- SCTransform(xenium_obj, assay = "Xenium")
+xenium_obj <- RunPCA(xenium_obj, npcs = 30, features = rownames(xenium_obj))
+xenium_obj <- RunUMAP(xenium_obj, dims = 1:30)
+  
+# Identify clusters of cells
+xenium_obj <- FindNeighbors(xenium_obj, reduction = "pca", dims = 1:30)
+xenium_obj <- FindClusters(xenium_obj, resolution = 0.3)
+
 # Function to compute each cell's area and correlate it with clusters
-get_cell_area_and_correlate <- function(xenium_obj) {
+get_cell_area_and_correlate <- function(xenium_obj, cluster = "seurat_clusters",plot_options= "Violin", add_p_val = F,exc_cell_type = F) {
+  # This function gets processed Xenium objects and give several plot outputs
   
-  xenium_obj[['area']] <- paste0("(",xenium_read[['centroids']][['x']],",",xenium_read[['centroids']][['y']],")")
-  
-  # Run UMAP to reduce dimensions for visualization
-  xenium_obj <- SCTransform(xenium_obj, assay = "Xenium")
-  xenium_obj <- RunPCA(xenium_obj, npcs = 30, features = rownames(xenium_obj))
-  xenium_obj <- RunUMAP(xenium_obj, dims = 1:30)
-  
-  # Identify clusters of cells
-  xenium_obj <- FindNeighbors(xenium_obj, reduction = "pca", dims = 1:30)
-  xenium_obj <- FindClusters(xenium_obj, resolution = 0.3)
-  
-  # Plot UMAP, colored by cell area ???
-  # Scatter plot
-  FeaturePlot(xenium_obj, features = rownames(xenium_obj))
-  # Violin plot
-  VlnPlot(xenium_obj, features = rownames(xenium_obj))
-  
-  # Plot UMAP, colored by clusters
-  DimPlot(xenium_obj)
-  
-  # correlation ???
-  ClusterAreaCorr <- cor.test(xenium_obj[['area']], xenium_obj[['seurat_clusters']], method = "spearman")
+  # get area for each cell
+  xenium_obj[['cell_area']] <- sapply(xenium_obj@images[["fov"]]@boundaries[["segmentation"]]@polygons,function(x) x@area)
+
+  # one can change a cell_type or another metdata column data with seurat_clusters
+
+                                      
+  # We have violin, boxplot, violin + boxplot options
+
+  # If add_p_val is True add pairwise comparison     
+
+  # One can exclude several clusters / Cell Types by using exc_cell_type
+                                      
+                                     
+                
   
   # Return the modified Seurat object, which now includes cell area metadata
-  # and clustering information
   return(xenium_obj)
 }
 
